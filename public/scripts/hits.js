386879a5e1ea996084c3ed996e3151dd3cce00bc
ax-x3 <x3amethyst@gmail.com>
2026-08-06 14:11:41 -0400

    fix: broken hit counter formatting

--- public/scripts/hits.js
+++ public/scripts/hits.js
@@ -18,8 +18,9 @@ async function updateHits() {
         }
         const resJSON = await res.json();
         const hits = resJSON.hits;
-        const hitsFixedLength = hits.padStart(8, '?');
-        document.getElementById("hitcount").innerHTML = hitsFixedLength.replaceAll("?", "<span class='text num pad'>?</span>");
+        const hitsFixedLength = String(hits).padStart(8, '?');
+        const hitsFormatted = hitsFixedLength.replaceAll("?", "<span class='text num pad'>?</span>");
+        document.getElementById("hitcount").innerHTML = hitsFormatted;
     } catch (error) {
         console.error(error);
     }
