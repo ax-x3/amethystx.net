f6f13ce27b07d5de62f6b3ac0ba299ed6488d9d9
ax-x3 <x3amethyst@gmail.com>
2026-08-22 03:28:48 -0400

    feat: add new titles

--- public/scripts/greetz.js
+++ public/scripts/greetz.js
@@ -11,9 +11,7 @@ function updateGreeting() {
 }
 function updateFunTitle() {
     const titles = [
-        "<span style='color: #f00'>f</span><span style='color: #fa0'>i</span><span style='color: #ff0'>l</span><span style='color: #0b0'>l</span><span style='color: #66f'>e</span><span style='color: #a0f'>d</span> <span style='color: #ff0'>w</span><span style='color: #fff'>i</span><span style='color: #a0f'>t</span><span style='color: #555'>h</span> <span style='color: #89f'>p</span><span style='color: #f88'>r</span><span style='color: #fff'>i</span><span style='color: #f88'>d</span><span style='color: #89f'>e</span>",
         "not actually a party",
-        "open for maintenance",
         "lorem ipsum or something",
         "population: 1",
         "open 24 hours",
@@ -30,17 +28,23 @@ function updateFunTitle() {
         "powered by stackoverflow",
         "this site uses no cookies",
         "#1 webkit hater",
+        "#1 safari hater",
         "a quiet corner of the internet",
+        "a weird corner of the internet",
+        "a purple corner of the internet",
         "200 ok",
         "internet participation award",
         "a web-browsing rest stop",
         "my home on the internet",
+        "your home on the internet",
         "a shout into the void",
+        "a cry into the dark",
         "dear internet stranger,",
         "@everyone",
         "greetings, traveller",
         "welcome, stranger",
         "howdy, surfer of the web",
+        "hello, voyager",
         "one of the sites of all time",
         "independent webber",
         "$ cat intro.txt",
@@ -51,15 +55,26 @@ function updateFunTitle() {
         "beware of cat",
         "beware of hyena",
         "human generated",
+        "i prompted my brain for this",
         "inspiration is an act of love",
         "lonely webmasters near you",
         "lonely furries near you",
+        "lonely artists near you",
+        "<span style='color: #f00'>f</span><span style='color: #fa0'>i</span><span style='color: #ff0'>l</span><span style='color: #0b0'>l</span><span style='color: #66f'>e</span><span style='color: #a0f'>d</span> <span style='color: #ff0'>w</span><span style='color: #fff'>i</span><span style='color: #a0f'>t</span><span style='color: #555'>h</span> <span style='color: #89f'>p</span><span style='color: #f88'>r</span><span style='color: #fff'>i</span><span style='color: #f88'>d</span><span style='color: #89f'>e</span>",
         "better than doomscrolling",
         "thanks for checking me out",
-        "don't read my blog",
+        "use code AIVI for 20% off",
         "commit and sync",
-        "headpats accepted here"
+        "headpats accepted here",
+        "artwork sold here",
+        "covered in bite marks",
+        "you're my favorite visitor btw",
+        "all i got was this lousy title",
+        "you going up or down?",
+        "you a cop?",
+        "you seem like a good one",
+        "what are you lookin' at?"
     ];
-    // document.getElementById("funTitle").innerHTML = titles[titles.length - 1];
-    document.getElementById("funTitle").innerHTML = titles[Math.floor(Math.random() * titles.length)];
+    document.getElementById("funTitle").innerHTML = titles[titles.length - 1];
+    // document.getElementById("funTitle").innerHTML = titles[Math.floor(Math.random() * titles.length)];
 }
